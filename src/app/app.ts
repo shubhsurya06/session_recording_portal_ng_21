f152a3bad61c5f3f4f34e5f2337b42ecd59256fe
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-12-29 21:49:21 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('session_recording_portal_ng_21');
+}
